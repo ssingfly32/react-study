acb3ae6dbdd483092df403cce7f5878d703c08e5
ssingfly32 <ssingfly3232@gmail.com>
2025-03-08 00:36:11 +0900

    useEffect part1

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,40 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  
+  // 리액트가 지켜볼 dependency가 없어서 최초 렌더링 됐을 때만 특정 작업 수행 (component mount)
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  
+  // 특정 prop, state가 바뀔 때
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  
+  // dependency는 배열이기 때문에 여러 개 감시 가능. 둘 중 하나라도 업데이트 될 때마다 특정 작업 수행
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+       <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
