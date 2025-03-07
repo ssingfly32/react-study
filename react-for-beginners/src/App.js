209b41f7e0d879082b9de27b377e541902573934
ssingfly32 <ssingfly3232@gmail.com>
2025-03-08 00:45:44 +0900

    useEffect part2:clean up

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,40 +1,20 @@
 import {useState, useEffect} from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  
-  // 리액트가 지켜볼 dependency가 없어서 최초 렌더링 됐을 때만 특정 작업 수행 (component mount)
+function Hello() {
   useEffect(() => {
-    console.log("I run only once.");
+    console.log("hi :)");
+    return () => console.log("bye :(");
   }, []);
-  
-  // 특정 prop, state가 바뀔 때
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
+  return <h1>Hello!</h1>;
+}
 
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  
-  // dependency는 배열이기 때문에 여러 개 감시 가능. 둘 중 하나라도 업데이트 될 때마다 특정 작업 수행
-  useEffect(() => {
-    console.log("I run when keyword & counter change");
-  }, [keyword, counter]);
-
+function App() {
+  const [isVisible, setIsVisible] = useState(false);
+  const handleTextVisible = () => setIsVisible(current => !current);
   return (
     <div>
-       <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {isVisible ? <Hello /> : null}
+      <button onClick={handleTextVisible}>{isVisible ? "hide" : "show"}</button>
     </div>
   );
 }
